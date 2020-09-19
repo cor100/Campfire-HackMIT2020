645566231e05d71059c628e2c6ead8bb45bd2fd9
vivianyunwang <vivianyunwang@gmail.com>
2020-09-19 15:09:50 -0700

    Initial Commit

---
+++ Gif/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gif
+//
+//  Created by Vivian Wang on 9/19/20.
+//  Copyright © 2020 Vivian Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
